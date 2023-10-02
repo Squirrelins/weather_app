cf6c9cd4667dbe1b9ec9ffb6384d0cbbe138bb73
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-02 09:15:22 -0400

    New file to practice and develop with.

---
+++ whileloops.py
@@ -0,0 +1,7 @@
+# Practice with while loops from Chapter 7 of "Python Crash Course"
+
+# input() works by pausing your program and waiting for the user to enter some text. Once Python receives the user's input, it stores it in a variable to make it convenient for you to work with.
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
