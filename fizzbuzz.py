4e0c69636a5c601e801fc421f521fc8882bbf5d8
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-04 18:52:18 -0400

    Added a file

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+# Write a python program that integreateas the integers from 1 to 50. 
+
+for i in range(1, 51):
+    if i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    
