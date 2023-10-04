8fccf7251c2a32f706e352c9fc5bc7439022e958
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-04 19:11:58 -0400

    worked on project

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-# Write a python program that integreateas the integers from 1 to 50. 
+# Write a python program that integreateas the integers from 1 to 50.
 
 for i in range(1, 51):
     if i % 5 == 0 and i % 3 == 0:
@@ -9,4 +9,3 @@ for i in range(1, 51):
         print("Buzz")
     else:
         print(i)
-    
