e2c3060f9d263081bd6f6647bdf8520a8c314a51
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-07 08:17:35 -0400

    updates

---
+++ homework.py
@@ -0,0 +1,8 @@
+# Write an program that asks for user input for a student's name, the courses they are taking, and their quiz scores. Store this information in a dictionary. Print the student's average quiz score and all of their information neatly.
+
+students = {'name': 'Daniel', 'courses': ['Python', 'SQL', 'Math'], 'grade': ['100', '100', '90']} # create dictionary
+
+print(students) # print dictionary
+print(students['name']) # print name
+print(students.get('name')) # print name
+
