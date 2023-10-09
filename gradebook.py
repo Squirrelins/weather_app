b9901b79faa573e95fda26596e5a4ee79c538833
Squirrelins <137178623+Squirrelins@users.noreply.github.com>
2023-10-08 21:33:54 -0400

    updates

---
+++ gradebook.py
@@ -0,0 +1,14 @@
+# Gradebook using dictionaries
+
+print("Welcome to the school of hard knocks!")
+print("This is the gradebook program.")
+
+# Create a dictionary to hold the students and grades
+
+students = {
+    'student_1': {
+        'first_name': '',
+        'last_name': '',
+        'course_grade': ''
+        },
+        }
