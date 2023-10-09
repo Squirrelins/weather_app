999f806029ddbb915f71905ded308b2a3288fc5a
Squirrelins <137178623+Squirrelins@users.noreply.github.com>
2023-10-09 07:59:16 -0400

    update

---
+++ gradebooktest.py
@@ -0,0 +1,16 @@
+students = {
+    'student_id': 100000 + len(students) + 1,
+    'first_name': input("Enter the student's first name: "),
+    'last_name': input("Enter the student's last name: "),
+    'courses': [],
+}
+courses = {
+    'course_no': 100 + len(courses) + 1,
+    'course_name': input("Enter the course name: "),
+    'professor': input("Enter the professor's name: "),
+}
+course_grade = {
+    
+}
+
+
