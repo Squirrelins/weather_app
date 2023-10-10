439452a06f7b7ce8f511277c9692b45b66168c27
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-09 21:17:34 -0400

    updates

---
+++ fileprogramming.py
@@ -0,0 +1,34 @@
+# Files introduced in this chapter using loops and processes. 
+# Files and directories should be able to be navigated through directories and drives.
+# Files should be able to be copied, moved, and deleted.
+# Using the with Statement to Open Files
+# The with statement is a Pythonic way to open files.
+# The with statement makes sure that the file is closed when the block inside the with statement is exited.
+# The with statement is about exception handling.
+# You can catch exceptions and still call the close method.
+# os library
+# The os library is used to navigate through directories and drives.
+# The os library is used to copy, move, and delete files.
+# The os library is used to get information about files.
+# The os library is used to get information about directories.
+# The os library is used to create and delete directories.
+# The os library is used to change the current directory.
+# The os library is used to get information about drives.
+# The os library is used to get information about the system.
+# The os library is used to execute programs.
+# The os library is used to get environment variables.
+# The os library is used to get the current process ID.
+# The os library is used to get the current user ID.
+# The os library is used to get the current group ID.
+# The os library is used to get the current working directory.
+# The os library is used to get the current working drive.
+# The os library is used to get the current operating system.
+# The os library is used to get the current platform.
+# The os library is used to get the current processor.
+# The os library is used to get the current path.
+# The os library is used to get the current path separator.
+# The os library is used to get the current line separator.
+# The os library is used to get the current environment.
+# The os library is used to get the current environment separator.
+# The os library is used to get the current environment value.
+
