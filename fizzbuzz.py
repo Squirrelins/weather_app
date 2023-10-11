ef7078bfa24ab39fccd9e3dddafd9ef8e65f7634
Daniel Derflinger <137178623+Squirrelins@users.noreply.github.com>
2023-10-11 08:24:01 -0400

    updates

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-# Write a python program that integreateas the integers from 1 to 50.
+# Fizz Buzz is a classic programming problem. Here is its slightly modified version from class.
 
 for i in range(1, 51):
     if i % 5 == 0 and i % 3 == 0:
